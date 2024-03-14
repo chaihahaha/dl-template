e10700c87b3cbd99b4880f10ed2bd2faac034271
chaihahaha <chai836275709@gmail.com>
2024-03-14 20:48:40 +0800

    add packplot decorator to load redrawable pictures

---
+++ plot_utils.py
@@ -0,0 +1,81 @@
+import inspect
+from PIL import Image
+import matplotlib.pyplot as plt
+import dill
+from functools import wraps
+import numpy as np
+
+def packplot(plotf):
+    @wraps(plotf)
+    def decorated_plotf(*args, **kwargs):
+        import dill
+        import inspect
+        from PIL import Image
+        n_pos_args = len(args)
+        all_arg_names = list(inspect.signature(plotf).parameters.keys())
+        pos_arg_names = all_arg_names[:n_pos_args]
+        for i in range(n_pos_args):
+            kwargs[pos_arg_names[i]] = args[i]
+        assert 'filename' in kwargs
+        filename = kwargs['filename']
+
+        ret = plotf(**kwargs)
+
+        try:
+            plot_function_code = inspect.getsource(plotf)
+        except OSError:
+            return ret
+        plot_function_name = plotf.__name__
+        img = Image.open(filename)
+        packed_info = (kwargs, plot_function_code, plot_function_name)
+        packed_bytes = dill.dumps(packed_info)
+        img.save(filename, exif=packed_bytes)
+        return ret
+
+    return decorated_plotf
+
+#def plot_random(x, y, filename, **kwargs):
+#    plot_args = locals()
+#    import dill
+#    import inspect
+#    from PIL import Image
+#
+#    import matplotlib.pyplot as plt
+#    plt.plot(x, y)
+#    plt.show()
+#    plt.savefig(filename)
+#
+#    try:
+#        plot_function_code = inspect.getsource(plot_random)
+#        plot_function_name = 'plot_random'
+#        img = Image.open(filename)
+#        packed_info = (plot_args, plot_function_code, plot_function_name)
+#        packed_bytes = dill.dumps(packed_info)
+#        img.save(filename, exif=packed_bytes)
+#    except OSError:
+#        return
+#    return
+
+@packplot
+def plot_test_dec(x, y, filename):
+    plt.plot(x, y)
+    plt.show()
+    plt.savefig(filename)
+    return 0
+
+def retrive_plot(filename):
+    img = Image.open(filename)
+    img.load()
+    packed_info = dill.loads(img.info['exif'][6:])
+    plot_args, plot_function_code, plot_function_name = packed_info
+    filename = plot_args['filename']
+    plot_args['filename'] = f'tmp-{str(hash(filename))}' + filename
+    exec(plot_function_code)
+    exec(plot_function_name + '(**plot_args)')
+
+if __name__ == '__main__':
+    x = np.random.rand(10)
+    y = np.random.rand(10)
+    #plot_random(x, y, 'test.png')
+    plot_test_dec(x, y, 'test.png')
+    retrive_plot('test.png')
