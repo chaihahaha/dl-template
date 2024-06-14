900d71069c8be54722a59dbfb81250e790374236
chaihahaha <chai836275709@gmail.com>
2024-06-14 21:47:40 +0800

    backup instead of avoiding overwrite

--- never_overwrite.py
+++ never_overwrite.py
@@ -2,15 +2,18 @@ import os
 import datetime
 import builtins
 import uuid
+import pathlib
 
 original_open = builtins.open
 
 def safe_open(filename, mode='r', **kwargs):
     if 'w' in mode and os.path.isfile(filename):
-        suffix = datetime.datetime.utcnow().strftime('_%Y_%m_%d_%H_%M_%S.%f') + uuid.uuid4().hex
-        newname = filename + suffix
+        p = pathlib.Path(filename)
+        suffix = str(os.path.getctime(filename)) + datetime.datetime.utcnow().strftime('_%Y_%m_%d_%H_%M_%S.%f_') + uuid.uuid4().hex
+        newname = str(p.with_stem(f"{p.stem}_{suffix}"))
+        os.rename(filename, newname)
     else:
         newname = filename
-    return original_open(newname, mode=mode, **kwargs)
+    return original_open(filename, mode=mode, **kwargs)
 
 builtins.open = safe_open
