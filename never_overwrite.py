01910036a1cfe1b7478fdc79669d5cb0a12bdb7f
chaihahaha <chai836275709@gmail.com>
2024-06-24 10:41:05 +0800

    Never overwrite only if disk free space is high enough.

--- never_overwrite.py
+++ never_overwrite.py
@@ -3,11 +3,12 @@ import time
 import builtins
 import uuid
 import pathlib
+import shutil
 
 original_open = builtins.open
 
 def safe_open(filename, mode='r', **kwargs):
-    if 'w' in mode and os.path.isfile(filename):
+    if 'w' in mode and os.path.isfile(filename) and shutil.disk_usage('/').free > 2**34:
         p = pathlib.Path(filename)
         suffix = f"{os.path.getctime(filename)}_{time.time()}_{uuid.uuid4().hex}"
         newname = str(p.with_stem(f"{p.stem}_{suffix}"))
